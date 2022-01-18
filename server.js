ea04ca741eeec8b46f4e2426e5800677021f0174
Carl Contreras <contrerascarl09@gmail.com>
2022-01-18 09:48:55 -0600

    created  a path to inputCheck

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,11 +9,71 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: 'Hello World'
-//     });
+// app.get('/api/candidates', (req, res) => {
+//     const sql = `SELECT * FROM candidates`;
+
+//     // get all candidates
+//     db.query(sql, (err, rows)=> {
+//         if (err) {
+//             res.status(500).json ({ error: err.message });
+//             return;
+//         }
+//         res.json({
+//             message: 'success',
+//             data: rows
+//         });
+//     })
 // });
+
+// get a single candidates
+// app.get('/api/candidate/:id', (req, res) => {
+//     const sql = `SELECT * FROM candidates WHERE id = ?`;
+//     const params = [req.params.id];
+
+//     // get a single candidates
+//     db.query(sql, params, (err, row)=> {
+//         if (err) {
+//             res.status(400).json ({ error: err.message });
+//             return;
+//         }
+//         res.json({
+//             message: 'success',
+//             data: row
+//         });
+//     })
+// });
+
+// delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result)=> {
+        if (err) {
+            res.statusMessage(400).json ({ error: err.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    })
+});
+
+// create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+});
+
 
 // .Default response for any other request (Not Found)
 app.use((req, res) => {
